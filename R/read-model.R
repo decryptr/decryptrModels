585f5acaee0346f8b71fcd6396b98d81e88360f3
Julio Trecenti <julio.trecenti@gmail.com>
2017-07-15 15:39:51 -0300

    wrong indices

--- R/read-model.R
+++ R/read-model.R
@@ -15,7 +15,7 @@ read_model <- function(model_name) {
   path <- system.file('keras', package = 'decryptrModels')
   files <- dir(path, full.names = TRUE)
   file_path <- files[grepl(model_name, files)]
-  labs <- models[grepl(model_name, models[['name']]), 'labs'][[1]]
+  labs <- models[grepl(model_name, models[['name']]),][['labs']][[1]]
   m <- list(
     labs = labs,
     model = keras::load_model_hdf5(file_path)
